917050942c7df014fc299d04701a1f01a6923016
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-04-27 15:37:52 -0300

    Refactor database schema and migration files

---
+++ prisma/migrations/20240427182321_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "text" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "authorId" INTEGER NOT NULL,
+    CONSTRAINT "Message_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
--- src/controller/chat.controller.js
+++ src/controller/chat.controller.js
@@ -17,8 +17,22 @@ export class ChatController {
         }
     }
     getAllMessage = async (req, res) => {
-        const allMessage = await this.repository.getAllMessageWithUser();
-        console.log(allMessage);
-        return res.status(200).json(allMessage);
+        try {
+            const allMessage = await this.repository.getAllMessageWithUser();
+            console.log(allMessage);
+            return res.status(200).json(allMessage);
+        } catch (error) {
+            console.error('Error while fetching all messages:', error);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+    deleteAllMessage = async (req, res) => {
+        try {
+            await this.repository.deleteAllMessage();
+            return res.status(204).send();
+        } catch (error) {
+            console.error('Error while deleting all messages:', error);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
     }
 }
