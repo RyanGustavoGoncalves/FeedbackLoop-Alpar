1cfe84c031829ba522efff0fc1da065d3ad774cc
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-04-29 20:39:30 -0300

    Refactor styling and functionality in style.css, main.js, and index.html

--- public/home/assets/scripts/main.js
+++ public/home/assets/scripts/main.js
@@ -1,10 +1,21 @@
 angular.module('FeedbackLoopHome', [])
     .controller('HomeController', function ($scope, $http, $timeout) {
         $scope.userData = JSON.parse(localStorage.getItem("user"));
+        $scope.inputUpdate = ""
         $scope.message = "";
+        $scope.modal = false;
+        $scope.id = ""
         $scope.data = [];
         $scope.chatContent = document.getElementById("chatContainer");
 
+
+        $scope.openModal = (id) => {
+            $scope.id = id;
+            $scope.modal = true;
+        }
+        $scope.closeModal = () => {
+            $scope.modal = false;
+        }
         $scope.submit = () => {
             $http.post("http://localhost:3000/api/message/send", {
                 message: $scope.message,
@@ -34,10 +45,9 @@ angular.module('FeedbackLoopHome', [])
                 });
         };
 
-        $scope.updateMessage = (id) => {
-            console.log($scope.inputUpdate);
-            $http.put(`http://localhost:3000/api/message/update/${id}`, {
-                message: $scope.inputUpdate
+        $scope.updateMessage = (value) => {
+            $http.put(`http://localhost:3000/api/message/update/${$scope.id}`, {
+                message: value
             })
                 .then((res) => {
                     $scope.loadMessages();
